cdc0a6a69689e6d2ccabdfc394b03671095949b0
Hazar Seven <hazar.seven@gazi.edu.tr>
2024-04-17 15:58:42 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,62 @@
+const bells = new Audio('./sounds/bell.wav'); 
+const startBtn = document.querySelector('.btn-start'); 
+const resetBtn = document.querySelector('.btn-reset');
+const pauseBtn = document.querySelector('.btn-pause');
+const session = document.querySelector('.minutes'); 
+let myInterval; 
+let state = true;
+
+const appTimer = () => {
+  const sessionAmount = Number.parseInt(session.textContent)
+
+  if(state) {
+    state = false;
+    let totalSeconds = sessionAmount * 60;
+
+    const updateSeconds = () => {
+      const minuteDiv = document.querySelector('.minutes');
+      const secondDiv = document.querySelector('.seconds');
+    
+      totalSeconds--;
+    
+      let minutesLeft = Math.floor(totalSeconds/60);
+      let secondsLeft = totalSeconds % 60;
+
+      
+     
+      if(secondsLeft < 10) {
+        secondDiv.textContent = '0' + secondsLeft;
+      } else {
+        secondDiv.textContent = secondsLeft;
+      }
+      minuteDiv.textContent = `${minutesLeft}`
+    
+      if(minutesLeft === 0 && secondsLeft === 0) {
+        bells.play()
+        clearInterval(myInterval);
+      }
+    }
+    myInterval = setInterval(updateSeconds, 1000);
+  } else {
+    alert('Session has already started.')
+  }
+}
+
+const resetTimer = () => {
+  clearInterval(myInterval);
+  state = true;
+  session.textContent = '25'; 
+  document.querySelector('.seconds').textContent = '00';
+}
+
+const pauseTimer = () => {
+  clearInterval(myInterval);
+  state = true;
+}
+
+
+
+startBtn.addEventListener('click', appTimer);
+resetBtn.addEventListener('click', resetTimer);
+pauseBtn.addEventListener('click', pauseTimer);
+
